7261a523ce2cbc4d267110bdbfc9cea5a6216d73
GnozL <gggonzalez000@gmail.com>
2025-10-21 23:35:46 -0700

    Part 3: Write unit tests

--- test_tictactoe.cpp
+++ test_tictactoe.cpp
@@ -12,8 +12,8 @@ void test_initial_state() {
     assert(game.getMoveCount() == 0);
 
     //All cells should be empty (' ')
-    for (int i = 1; i <= 3; i++) {
-        for (int j = 1; j <= 3; j++) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
             assert(game.getCell(i,j) == ' ');
         }
     }
